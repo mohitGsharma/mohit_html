4cf16db88ee92daf49122aae5581cd89711dc6be
mohitGsharma <mohitpsharma088@gmail.com>
2017-09-24 05:38:27 -0700

    php_pramid#2

---
+++ piramid_php/index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Mohit.......";
+echo "<br/>";
+echo " WELCOM ";
+ 
+echo "<br/><br/>";
+
+for($i=5;$i>=0;$i--)
+{
+    
+    for($j=0;$j<=$i;$j++)
+    {
+        echo $j. " ";
+    }
+    echo "<br/>";
+}
+echo "End";
+ ?>
