51b815d6cfac5680ac5c96909b95b571e2f8865a
mohitGsharma <mohitpsharma088@gmail.com>
2017-09-25 04:34:47 -0700

    html-Basic

---
+++ htm- basic/img.php
@@ -0,0 +1,6 @@
+<html>
+
+ Image<br/><br/>
+ <img src = "Pictures/mith" width = "100"  height = "100" /> 
+
+</html>
---
+++ htm- basic/link.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>My First Heading</h1>
+<h2>My First Heading</h2>
+<h3>My First Heading</h3>
+
+<p>My first paragraph.</p>
+<p>My another  paragraph.</p>
+
+
+<a href= "www.w3school.com"> this is link page</a>
+
+</body>
+</html>
