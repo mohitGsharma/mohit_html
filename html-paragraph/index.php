6b65b507503e74dc687cbe51364571b9c53899c6
mohitGsharma <mohitpsharma088@gmail.com>
2017-09-26 05:08:21 -0700

    html-paragraph

---
+++ html-paragraph/index.php
@@ -0,0 +1,8 @@
+<html>
+
+<p> this is paragraph
+<p> this is paragraph
+<p> this is <br/> paragraph <br/> with line break <hr> </p>
+
+<a href = "p1.php"> first para</a>
+</html>
---
+++ html-paragraph/p1.php
@@ -0,0 +1,29 @@
+<html>
+
+<p >
+<i> 
+
+My bonnie lies over the ocean. <br/>
+
+My bonnie lies over the sea.<br/>
+
+my bonnie lies over the ocean. <br/>
+
+oh!bring back  my bonnie to me. <br/>
+
+</i>
+</p>
+
+<pre style =" color:red" > 
+
+My bonnie lies over the ocean. 
+
+My bonnie lies over the sea.
+
+my bonnie lies over the ocean. 
+
+oh!bring back  my bonnie to me. 
+
+</pre>
+
+</html>
