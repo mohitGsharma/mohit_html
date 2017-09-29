bed2fba5810507c3fa190394ebe06197efd5f410
mohitGsharma <mohitpsharma088@gmail.com>
2017-09-29 03:26:11 -0700

    html-comment

---
+++ html- comment/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<!-- this is comment -->
+
+<p> this is paragraph </p> 
+
+<!-- Remember to add Info-->
+
+<!-- Do not display now
+<img border="0" src="mountain.jpg" />
+-->
+
+</body>
+</html>
