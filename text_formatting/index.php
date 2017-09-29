045e150bf11294058d4fb0935c40496fe244beb2
mohitGsharma <mohitpsharma088@gmail.com>
2017-09-29 02:42:42 -0700

    text-formatting

---
+++ text_formatting/index.php
@@ -0,0 +1,29 @@
+<html>
+<body style = " background-color: pink; " >
+
+<p><b>This is boldM</b></p>
+
+<p><i>this is italic</i></p>
+ 
+<p> <strong> this is Strong  </strong>
+
+
+ <p> <em> this is emphasized </em></p>
+
+<p> THis is <sub> subscript </sub>    this is <sup> superscript </sup> </p>
+
+<h2> html <small> small </small>  formatting  </h2>
+
+<h2> html <mark> Mark </mark>  formatting</h2>
+
+<p>   My fav color is <del> blue  </del>   red </p> 
+
+<p>  My fav  is <ins>color  </ins>  is  red  </p> 
+
+<p> This is  <sub>  Subscript </sub> text </p>
+
+<p>  this is <sup>  Superscript </sup>  text </p> 
+
+ 
+<body>
+</html>
