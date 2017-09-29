5830d295e4e8a3ec34ade9353e8e7db24ff0ece1
mohitGsharma <mohitpsharma088@gmail.com>
2017-09-29 02:41:36 -0700

    html-style

---
+++ html-style/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body style="background-Color:powderblue;" >
+
+
+<h1 style = "color:grey;font-family:verdana;" >THis is a heading</h1>
+
+<p style  = "font-size:100%;text-align:center;" >I am normal</p>
+
+<p style="color:red;"><i>I am red</i></p>
+
+<p style="color:blue;font-size:300%;"><u>I am blue</u></p>
+
+<p style="font-size:50px;">I am big</p>
+
+</body>
+</html>
